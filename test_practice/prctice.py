8999f195e53002de059b65555ef58a7cff7e73f5
samuel-1202 <shcho115@gmail.com>
2025-10-22 14:04:10 +0900

    Add practice_chap_1.ipynb - Effective Python Chapter 1 practice notebook

---
+++ test_practice/prctice.py
@@ -0,0 +1,14 @@
+import sys
+print(sys.version_info)
+print(sys.version)
+
+def do_str(bytes_or_str):
+    if isinstance(bytes_or_str, bytes):
+        value = bytes_or_str.decode('utf-8')
+    else:
+        value = bytes_or_str
+    return value # str 인스턴스
+
+print(repr(do_str(b'foo')))
+print(repr(do_str('bar')))
+print(repr(do_str(b'foo\xffbar')))
